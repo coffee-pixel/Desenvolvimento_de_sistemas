a654716c0fdbc058abb13f508ef4446c5b110d80
coffee-pixel <cauebarbosa@hotmail.com>
2020-02-10 13:11:27 -0300

    agora esta pronto

--- Logo.java
+++ Logo.java
@@ -5,25 +5,39 @@
  */
 package logo;
 import  javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author PICHAU
  */
 public class Logo extends JFrame{
-
+    JLabel texto1, texto2;
     public Logo(){
         super("logo");
-        ImageIcon icone = new ImageIcon("overwatch.gif");
+        Container tela = getContentPane();
+        ImageIcon icone = new ImageIcon("logo2.png");
         setIconImage(icone.getImage());
-        setSize(300,150);
+        tela.setBackground(Color.lightGray);
+        setSize(500,300);
         setVisible(true);
-        setLocationRelativeTo(null);
-    }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+        setLocationRelativeTo(null); 
+        texto1 = new JLabel("Primeiro teste com JLabel");
+        texto2 = new JLabel("Segundo teste com JLabel");
+        texto1.setBounds(50,20,80,20);
+        texto2.setBounds(50,30,80,20);
+        texto1.setForeground(Color.darkGray);
+        texto2.setForeground(Color.blue);
+        texto1.setFont(new Font("Arial", Font.BOLD, 10));
+        texto2.setFont(new Font("arial", Font.BOLD, 10));
         
+        tela.add(texto1);
+        tela.add(texto2);
+    }
+    
+
+
+
+    public static void main(String[] args) {
         Logo app = new Logo();
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // TODO code application logic here
